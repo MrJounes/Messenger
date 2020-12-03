ab0c0627649916444f1d052e98832bd70c206cdf
Igor Dikan <IgorDikan@Igors-Mac-mini.local>
2020-12-03 19:54:44 +0400

    Работа с LoginViewController

---
+++ Messenger/Controllers/Login/RegisterViewController.swift
@@ -0,0 +1,16 @@
+//
+//  RegisterViewController.swift
+//  Messenger
+//
+//  Created by Igor Dikan on 03.12.2020.
+//
+
+import UIKit
+
+class RegisterViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .link
+    }
+}
