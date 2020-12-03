6e0a52bb2511d511356a0fdeff052d213bfe8337
Igor Dikan <IgorDikan@Igors-Mac-mini.local>
2020-12-03 18:28:42 +0400

    Создание структуры приложения

--- Messenger/Controllers/ConversationsViewController.swift
+++ Messenger/Controllers/ConversationsViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  ConversationsViewController.swift
 //  Messenger
 //
 //  Created by Igor Dikan on 03.12.2020.
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ConversationsViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
